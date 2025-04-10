dc89bfe13704698d8d705ff304c387ce98571b53
klyg9 <kely.gbedemah24@icloud.com>
2025-04-10 02:09:41 +0000

    program5

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
+#include <cstdlib>   // For rand() and srand()
+#include <ctime>     // For time()
+
 using namespace std;
 
 int main() {
-    const int TOTAL_ROWS = 10;
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+    int numberToGuess = rand() % 100 + 1;  // Random number between 1 and 100
 
-    // Pattern A - Increasing
-    cout << "Pattern A" << endl;
-    for (int i = 1; i <= TOTAL_ROWS; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "+";
+    int userGuess;
+    int guessCount = 0;
+
+    cout << "Guess the number (between 1 and 100): ";
+
+    // Loop until user guesses correctly
+    do {
+        cin >> userGuess;
+        guessCount++;
+
+        if (userGuess > numberToGuess) {
+            cout << "Too high, try again: ";
+        } else if (userGuess < numberToGuess) {
+            cout << "Too low, try again: ";
         }
-        cout << endl;
-    }
+    } while (userGuess != numberToGuess);
 
-    cout << endl;
-
-    // Pattern B - Decreasing
-    cout << "Pattern B" << endl;
-    for (int i = TOTAL_ROWS; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            cout << "+";
-        }
-        cout << endl;
-    }
+    cout << "Congratulations! You guessed the number in " << guessCount << " attempts." << endl;
 
     return 0;
 }
